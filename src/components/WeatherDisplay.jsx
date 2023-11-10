280670b95cc0a965d888cf7a67fac267d9caa86b
Mina <Mina@example.com>
2023-11-10 15:20:57 +0100

    friday edit on changing pages

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -53,104 +53,103 @@ const WeatherDisplay = () => {
   };
 
   return (
-    <div className="main">
-      <div className="input-container">
-        <h1 className="title">Wetteranzeige</h1>
-        <input
-          className="city-input"
-          type="text"
-          value={city}
-          onChange={handleCityChange}
-          placeholder="Stadt eingeben"
-        />
-        <button className="get-weather-button" onClick={handleGetWeather}>
-          Wetter abrufen
-        </button>
+    <div className=''>
+      <div className=''>
+        <h1 className='weather-title'>Weather Forcasting</h1>
+        <div className='search'>
+          <input
+            type='text'
+            value={city}
+            onChange={handleCityChange}
+            placeholder='city ...'
+          />
+          <button type="submit" onClick={handleGetWeather}>Search</button>
+        </div>
       </div>
-      {loading && <p>Lade Wetterdaten...</p>}
+      {loading && <p>Loading data...</p>}
 
       {error && <p>{error}</p>}
 
       {weather && (
-        <div className="card">
+        <div className='weather-main'>
           <div>
             {(weather.weather[0].main === "Clouds" ||
               weather.weather[0].main === "Mist") && (
-              <div className="container">
-                <div className="cloud front">
-                  <span className="left-front"></span>
-                  <span className="right-front"></span>
+              <div className='weather-input-container'>
+                <div className='cloud front'>
+                  <span className='left-front'></span>
+                  <span className='right-front'></span>
                 </div>
-                <span className="sun"></span>
-                <div className="cloud back">
-                  <span className="left-back"></span>
-                  <span className="right-back"></span>
+                <span className='sun'></span>
+                <div className='cloud back'>
+                  <span className='left-back'></span>
+                  <span className='right-back'></span>
                 </div>
               </div>
             )}
 
             {weather.weather[0].main === "Clear" && (
-              <div className="container">
-                <span className="sun sunshine"></span>
-                <span className="sun"></span>
+              <div className='container'>
+                <span className='sun suweather nshine'></span>
+                <span className='sun'></span>
               </div>
             )}
 
             {(weather.weather[0].main === "Rain" ||
               weather.weather[0].main === "Drizzle") && (
-              <div className="container">
-                <div className="rain-container">
-                  <span className="rain"></span>
-                  <span className="rain"></span>
-                  <span className="rain"></span>
+              <div className='container'>
+                <div className='rain-container'>
+                  <span className='rain'></span>
+                  <span className='rain'></span>
+                  <span className='rain'></span>
                 </div>
-                <div className="cloud back">
-                  <span className="left-back"></span>
-                  <span className="right-back"></span>
+                <div className='cloud back'>
+                  <span className='left-back'></span>
+                  <span className='right-back'></span>
                 </div>
-                <div className="cloud front">
-                  <span className="left-front"></span>
-                  <span className="right-front"></span>
+                <div className='cloud front'>
+                  <span className='left-front'></span>
+                  <span className='right-front'></span>
                 </div>
               </div>
             )}
             {weather.weather[0].main === "Snow" && (
-              <div className="container">
-                <div className="rain-container">
-                  <span className="snow"></span>
-                  <span className="snow"></span>
-                  <span className="snow"></span>
+              <div className='container'>
+                <div className='rain-container'>
+                  <span className='snow'></span>
+                  <span className='snow'></span>
+                  <span className='snow'></span>
                 </div>
-                <div className="cloud back">
-                  <span className="left-back"></span>
-                  <span className="right-back"></span>
+                <div className='cloud back'>
+                  <span className='left-back'></span>
+                  <span className='right-back'></span>
                 </div>
-                <div className="cloud front">
-                  <span className="left-front"></span>
-                  <span className="right-front"></span>
+                <div className='cloud front'>
+                  <span className='left-front'></span>
+                  <span className='right-front'></span>
                 </div>
               </div>
             )}
           </div>
-          <div className="card-header">
-            <span>{weather.name}</span>
-            <span>{weather.weather[0].description}</span>
-            <span>Luftfeuchtigkeit: {weather.main.humidity}%</span>
+          <div className='card-header'>
+            <p className='weather-date'>{new Date().toDateString()}</p>
+            <h2>{weather.name}</h2>
+            <h5>{weather.weather[0].description}</h5>
+            <span>Humidity {weather.main.humidity}%</span>
           </div>
-          <span className="temp">
+          <span className='temp'>
             {weather && isCelsius
-          ? convertToCelsius(weather.main.temp) + "°C"
-          : convertToFahrenheit(weather.main.temp) + "°F"}
+              ? convertToCelsius(weather.main.temp) + "°C"
+              : convertToFahrenheit(weather.main.temp) + "°F"}
           </span>
-          <div className="temp-scale">
-            <span   
-              onClick={toggleTemperatureScale}
-              style={{ cursor: "pointer" }}
-            >
-              {isCelsius ? "Celsius" : "Fahrenheit"}
-            </span>
-          </div>
-          <p className="date">{new Date().toDateString()}</p>
+
+          <span
+            className='temp-scale'
+            onClick={toggleTemperatureScale}
+            style={{ cursor: "pointer" }}
+          >
+            {isCelsius ? "to Fahrenheit" : "to Celsius"}
+          </span>
         </div>
       )}
     </div>
