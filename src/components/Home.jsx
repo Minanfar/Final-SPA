0913cdab7f5803353e60ad95252c8e4d873f96df
Mina <Mina@example.com>
2023-11-11 21:12:11 +0100

    home pages design+ news and weather are finaly finishedl

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,80 @@
-import React from "react";
+import React, { useReducer } from "react";
+import weather from "../assets/thunder.jpg";
+import news from "../assets/newspaper.jpg";
+import greece from "../assets/greece.jpg";
+import { Link } from "react-router-dom";
+import "./css/home.css";
+
+const initialState = {
+  weather: { isHovered: false, title: "Weather" },
+  news: { isHovered: false, title: "News" },
+  history: { isHovered: false, title: "History Events" },
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "HOVER":
+      return {
+        ...state,
+        [action.content]: { ...state[action.content], isHovered: true },
+      };
+    case "RESET":
+      return {
+        ...state,
+        weather: { ...state.weather, isHovered: false },
+        news: { ...state.news, isHovered: false },
+        history: { ...state.history, isHovered: false },
+      };
+    default:
+      return state;
+  }
+};
 
 function Home() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
-     Home
+      <div className='home-main-box'  >
+        <h1 className='home-title'>
+          Discover the past, stay informed about the present, and plan for the
+          future with our comprehensive SPA. Navigate through sections on
+          history, news, and weather seamlessly.
+        </h1>
+        <div className='home-img-container' >
+          <Link
+            to='./weather'
+           
+            onMouseEnter={() => dispatch({ type: "HOVER", content: "weather" })}
+            onMouseLeave={() => dispatch({ type: "RESET" })}
+          >
+            <img src={weather} alt='' className='home-img' />
+            {state.weather.isHovered && (
+              <p className='toggle-title' >{state.weather.title}</p>
+            )}
+          </Link>
+          <Link
+            to='./news'
+            onMouseEnter={() => dispatch({ type: "HOVER", content: "news" })}
+            onMouseLeave={() => dispatch({ type: "RESET" })}
+          >
+            <img src={news} alt='' className='home-img' />
+            {state.news.isHovered && (
+              <p className='toggle-title'>{state.news.title}</p>
+            )}
+          </Link>
+          <Link
+            to='./history'
+            onMouseEnter={() => dispatch({ type: "HOVER", content: "history" })}
+            onMouseLeave={() => dispatch({ type: "RESET" })}
+          >
+            <img src={greece} alt='' className='home-img' />
+            {state.history.isHovered && (
+              <p className='toggle-title'>{state.history.title}</p>
+            )}
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -5,6 +5,7 @@ const NewsCard = () => {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+ 
 
   const API_KEY = '2e9f19b0185b42ce96f13b82ca4576cc';
   const API_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
@@ -32,9 +33,10 @@ const NewsCard = () => {
   }, [API_URL]);
 
   return (
-    <div className="news-card">
-      <h2>Aktuelle Nachrichten</h2>
-      {loading && <p>Lade Nachrichten...</p>}
+    <div >
+      <h1 className='news-title'>𝒫𝓇𝑒𝓈𝓈 𝒯𝑜𝒹𝒶𝓎</h1>
+      
+      {loading && <p><i>loading news...</i></p>}
       {error && <p>{error}</p>}
       <div className="news-container">
         {news.map((article, index) => (
@@ -43,7 +45,7 @@ const NewsCard = () => {
             <h3>{article.title}</h3>
             <p>{article.description}</p>
             <a href={article.url} target="_blank" rel="noopener noreferrer">
-              Lesen Sie mehr
+           Read more ... 
             </a>
           </div>
         ))}
